344f3e25c60afc6cd8c247e144c26902bb8eb5fd
jackwang9988 <39379864+jackwang9988@users.noreply.github.com>
2020-09-18 17:19:46 -0700

    sums

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 # recursive sum function
 
+
+def sum_digits(n):
+    s = 0
+    while n:
+        s += n % 10
+        n //=10
+    return s
+
+
 def findSums2(original, start, end):
     for i in range(start, end, -1):
         if i % 2000 == 1:
@@ -27,23 +36,37 @@ def findSubset(nums, target, partial=[]):
 
 # main
 if __name__ == '__main__':
-    # infile = open('Numbers.txt', 'r')  # full list of numbers
-    infile = open('short.txt', 'r')  # short list of numbers to test with
+    infile = open('Numbers.txt', 'r')  # full list of numbers
+    # infile = open('short.txt', 'r')  # short list of numbers to test with
     lines = infile.readlines()
     infile.close()
 
     # creating list of numbers
     numbers = []
     index = 0
+    sums = []  # list of number sums
+    s138 = []
 
     for line in lines:
         line = line.strip()
         numbers.append(int(line))
 
+    print("List of Numbers and their digit sums:\n")
+    for number in numbers:
+        tmp = sum_digits(number)
+        sums.append(tmp)
+        if tmp == 138:
+            s138.append(number)
+        print("Number: {} | Sum: {}".format(number, tmp))
+    sums.sort()
+    print(sums)
+    print(s138)
+
     numbers.sort()
     smallest = min(numbers)
     total = 0
     index = 0
+
     print("\nList of Numbers:\n")
     for number in numbers:
         total += number
@@ -54,7 +77,7 @@ if __name__ == '__main__':
     iterations = 0
     solutions = {}
     # findSums(numbers, numbers)
-    findSums2(numbers, total, smallest)
+    # findSums2(numbers, total, smallest)
 
     # prints the solutions
     sol = 0
